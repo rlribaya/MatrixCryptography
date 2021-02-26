df3495f8950b8bd7b97c8a850fdb7f6faca98b5b
rlribaya <79568279+rlribaya@users.noreply.github.com>
2021-02-26 10:48:19 +0800

    Update MatrixCryptography.java
    small refactoring

--- MatrixCryptography.java
+++ MatrixCryptography.java
@@ -9,12 +9,11 @@ public class MatrixCryptography {
         // Ask for invertible matrix
         System.out.println("Please input a 3x3 invertible matrix");
         int matrixA[][] = new int[3][3];
-        arrayInput(matrixA);
+        arrayInput(matrixA, s);
         
         // Ask for message
         System.out.println("Please input a messasge:");
         String msg = s.nextLine().toUpperCase();
-        
         
         // Determine number of 1x3 matrices
         int x = msg.length() / 3;
@@ -37,62 +36,41 @@ public class MatrixCryptography {
             output[i/3] = multiply(matrix, matrixA);
         }
         
-        
-        
-        
-        /*for (int i = 0; i < msg.length(); i++) {
-            if (i%3 == 0 && i != 0) {
-                System.out.print("1x3 Array #" + (i/3) + ": ");
-                for (int z : message) System.out.print(z + "   ");
-                System.out.println();
-                output[i/3 - 1] = multiply(message, matrixA);
-                message = new int[3];
-            }
-            message[i%3] = (msg.charAt(i) == 32 ? 0 : msg.charAt(i) - 64);
-        }
-        
-        if (msg.length() % 3 != 0) {
-            System.out.print("1x3 Array #" + x + ": ");
-            for (int z : message) System.out.print(z + "   ");
-            output[x - 1] = multiply(message, matrixA);
-            System.out.println();
-        }
-        */
-        System.out.println("Encoded message: ");
+        // Print Encoded Message
+        System.out.println("Encoded Message: ");
         arrayDisplay(output);
         
         s.close();
     }
-    public static void arrayInput(int[][] matrix) {
-        Scanner s = new Scanner(System.in);
+    public static void arrayInput(int[][] matrix, Scanner s) {
         for (int i = 0; i < matrix.length; i++) {
             System.out.println("Please input row " + (i+1) + " (separate each value by a <space>):");
-            for (int j = 0; j < matrix[i].length; j++) {
+            
+            for (int j = 0; j < matrix[i].length; j++) 
                 matrix[i][j] = s.nextInt();
-            }
         }
     }
     public static int[] multiply(int matrixA[], int matrixB[][]) {
          int product[] = new int[matrixA.length];
-            for (int i = 0; i < matrixB[0].length; i++) {
-                for (int j = 0; j < matrixA.length; j++) {
+            for (int i = 0; i < matrixB[0].length; i++) 
+                for (int j = 0; j < matrixA.length; j++) 
                     product[i] += matrixA[j] * matrixB[j][i];
-                }
-            }
+                
         return product;
     }
+    
     // Overloaded arrayDisplay
     public static void arrayDisplay(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + "   ");
-            }
-        }
+        for (int i[] : matrix) 
+            for (int j : i ) 
+                System.out.print(j + "   ");
+        
+        System.out.println();
     }
     public static void arrayDisplay(int[] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            System.out.print(matrix[i]);
-        }
+        for (int i : matrix) 
+            System.out.print(i + "   ");
+        
         System.out.println();
     }
 }
